ef80878ed4afa4bd48bdb0f7d6a39481e1f17f70
Kanji Hara Neto <kneto@kneto-macbookpro.roam.corp.google.com>
2016-08-10 14:13:03 -0300

    split code

--- src/main/java/com/ciandt/poc/BigtableHelper.java
+++ src/main/java/com/ciandt/poc/BigtableHelper.java
@@ -32,6 +32,95 @@ public class BigtableHelper {
 
 	// Write some friendly greetings to Cloud Bigtable
 	private static final String[] GREETINGS = { "Hello World!", "Hello Cloud Bigtable!", "Hello HBase!" };
+	
+	public String allTasksBigtable() {
+		StringBuffer buffer = new StringBuffer();
+		// [START connecting_to_bigtable]
+		// Create the Bigtable connection, use try-with-resources to make sure
+		// it gets closed
+		try (Connection connection = BigtableConfiguration.connect(PROJECT_ID, INSTANCE_ID)) {
+
+			// The admin API lets us create, manage and delete tables
+			Admin admin = connection.getAdmin();
+			// [END connecting_to_bigtable]
+
+			// [START creating_a_table]
+			// Create a table with a single column family
+			HTableDescriptor descriptor = new HTableDescriptor(TableName.valueOf(TABLE_NAME));
+			descriptor.addFamily(new HColumnDescriptor(COLUMN_FAMILY_NAME));
+
+			log.info("Create table " + descriptor.getNameAsString());
+			admin.createTable(descriptor);
+			// [END creating_a_table]
+
+			// [START writing_rows]
+			// Retrieve the table we just created so we can do some reads and
+			// writes
+			Table table = connection.getTable(TableName.valueOf(TABLE_NAME));
+
+			// Write some rows to the table
+			log.info("Write some greetings to the table");
+			for (int i = 0; i < GREETINGS.length; i++) {
+				// Each row has a unique row key.
+				//
+				// Note: This example uses sequential numeric IDs for
+				// simplicity, but
+				// this can result in poor performance in a production
+				// application.
+				// Since rows are stored in sorted order by key, sequential keys
+				// can
+				// result in poor distribution of operations across nodes.
+				//
+				// For more information about how to design a Bigtable schema
+				// for the
+				// best performance, see the documentation:
+				//
+				// https://cloud.google.com/bigtable/docs/schema-design
+				String rowKey = "greeting" + i;
+
+				// Put a single row into the table. We could also pass a list of
+				// Puts to write a batch.
+				Put put = new Put(Bytes.toBytes(rowKey));
+				put.addColumn(COLUMN_FAMILY_NAME, COLUMN_NAME, Bytes.toBytes(GREETINGS[i]));
+				table.put(put);
+			}
+			// [END writing_rows]
+
+			// [START getting_a_row]
+			// Get the first greeting by row key
+			String rowKey = "greeting0";
+			Result getResult = table.get(new Get(Bytes.toBytes(rowKey)));
+			String greeting = Bytes.toString(getResult.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME));
+			log.info("Get a single greeting by row key");
+			log.info(rowKey + ": " + greeting);
+			// [END getting_a_row]
+
+			// [START scanning_all_rows]
+			// Now scan across all rows.
+			Scan scan = new Scan();
+
+			log.info("Scan for all greetings:");
+			ResultScanner scanner = table.getScanner(scan);
+			for (Result row : scanner) {
+				byte[] valueBytes = row.getValue(COLUMN_FAMILY_NAME, COLUMN_NAME);
+				log.info('\t' + Bytes.toString(valueBytes));
+				buffer.append('\t' + Bytes.toString(valueBytes));
+			}
+			// [END scanning_all_rows]
+
+			// [START deleting_a_table]
+			// Clean up by disabling and then deleting the table
+			log.info("Delete the table");
+			admin.disableTable(table.getName());
+			admin.deleteTable(table.getName());
+			// [END deleting_a_table]
+
+		} catch (IOException e) {
+			return e.toString();
+		}
+
+		return buffer.toString();
+	}
 
 	
 
